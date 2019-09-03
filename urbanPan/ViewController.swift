d59e6189f63457d57ace585c1fab27fa1142f221
UrbanSmash <urbansmash@gmail.com>
2019-09-04 00:38:09 +0100

    Initial Commit via Xcode 10.3

--- urbanPan/ViewController.swift
+++ urbanPan/ViewController.swift
@@ -13,8 +13,9 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     
     var player : AVAudioPlayer!
     
-    let sounds = ["Rest", "FN03", "FS03"]
-    var soundFileName = "FN03"
+    let sounds = ["Rest", "2-F3", "2-FS3", "2-G3", "2-GS3", "2-A3", "2-AS3", "2-B3", "2-C4", "2-CS4", "2-D4", "2-DS4", "2-E4", "2-F4", "2-FS4", "2-G4", "2-GS4", "2-A4", "2-AS4", "2-B4", "2-C5", "2-CS5", "2-D5", "2-DS5", "2-E5", "2-F5", "2-FS5", "2-G5", "2-GS5", "2-A5", "2-AS5", "2-B5", "2-C6"]
+    
+    var soundFileName = "2-F3"
     
     override func viewDidLoad() {
         super.viewDidLoad()
